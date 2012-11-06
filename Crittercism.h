bdd82095ba731ac2d2d64b8a15510b93999f8376
Rex Sheng <shengning@gmail.com>
2012-11-06 08:58:36 +0800

    update to 3.3.4

--- Crittercism.h
+++ Crittercism.h
@@ -31,24 +31,26 @@
 @property(assign) BOOL didCrashOnLastLoad;
 
 + (Crittercism*)sharedInstance;
++ (void) initWithAppID:(NSString *)_app_id;
++ (void) initWithAppID:(NSString *)_app_id andMainViewController:(UIViewController *)_mainViewController andDelegate:(id) critterDelegate;
+
+// Deprecated in v3.3.1
 + (void) initWithAppID:(NSString *)_app_id andKey:(NSString *)_keyStr andSecret:(NSString *)_secretStr;
++ (void) initWithAppID:(NSString *)_app_id andKey:(NSString *)_keyStr andSecret:(NSString *)_secretStr andMainViewController:(UIViewController *)_mainViewController;
+
 + (NSString *) getAppID;
-+ (NSString *) getKey;
-+ (NSString *) getSecret;
 + (void) configurePushNotification:(NSData *) deviceToken;
++ (int) getCurrentOrientation;
++ (void) setCurrentOrientation: (int)_orientation;
++ (void) setOptOutStatus: (BOOL) _optOutStatus;
++ (BOOL) getOptOutStatus;
+
++ (BOOL) logHandledException:(NSException *)exception;
++ (void) leaveBreadcrumb:(NSString *)breadcrumb;
 + (void) setAge:(int)age;
 + (void) setGender:(NSString *)gender;
 + (void) setUsername:(NSString *)username;
 + (void) setEmail:(NSString *)email;
 + (void) setValue:(NSString *)value forKey:(NSString *)key;
-+ (int) getCurrentOrientation;
-+ (void) setCurrentOrientation: (int)_orientation;
-+ (void) leaveBreadcrumb:(NSString *)breadcrumb;
-+ (void) setOptOutStatus: (BOOL) _optOutStatus;
-+ (BOOL) getOptOutStatus;
-
-// Beta Features: Email support@crittercism.com for access
-+ (BOOL) logHandledException:(NSException *)exception;
-
 @end
 
